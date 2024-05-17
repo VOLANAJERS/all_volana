c263ffea1c7a05316a515e772564ed8ab27486c8
jers22 <152739931+jers22@users.noreply.github.com>
2024-05-16 23:18:19 -0600

    upd

--- Volana/LiberacionEspacio/consulta_registros_eliminar.sql
+++ Volana/LiberacionEspacio/consulta_registros_eliminar.sql
@@ -1,6 +1,6 @@
-select * from cartera_distribuidor
-WHERE fecha < DATE_SUB(CURDATE(), INTERVAL 3 YEAR)  -- Registros con más de un año de antigüedad
-   AND (DAY(fecha) != 15  -- Excluir registros del día 15
-	and DAY(fecha) != DAY(LAST_DAY(fecha))) 
+DELETE from cartera_distribuidor
+WHERE fecha < DATE_SUB(CURDATE(), INTERVAL 1 YEAR)  -- Registros con más de un año de antigüedad
+    AND (DAY(fecha) != 15  -- Excluir registros del día 15
+	 and DAY(fecha) != DAY(LAST_DAY(fecha))) 
 -- AND id_distribuidor
 -- ORDER BY fecha
