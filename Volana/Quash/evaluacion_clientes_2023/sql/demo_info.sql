8bb76d540b8ff01d71d90c8adf577d15bab2d59b
jers22 <152739931+jers22@users.noreply.github.com>
2024-06-30 23:31:47 -0600

    upd

---
+++ Volana/Quash/evaluacion_clientes_2023/sql/demo_info.sql
@@ -0,0 +1,47 @@
+select 
+id as 'APPLICANT_ID', 
+monto_mensual as ingreso_mensual,
+gastos_familiares,
+otros_ingresos,
+replace(replace(replace(ingreso_neto,"$",""), ",",""), ".00","") as ingreso_neto,
+dependientes,
+ventas,
+FLOOR(DATEDIFF(CURDATE(), fecha_nacimiento) / 365.25) AS edad,
+monto_mensual,
+compras,
+delegacion,
+delegacion_negocio,
+escolaridad,
+estado,
+estado_civil,
+estado_negocio,
+sexo,
+sucursal,
+tipo_negocio,
+tipo_vivienda
+from cc_prospectos_demograficos
+
+UNION
+
+select 
+id as 'APPLICANT_ID', 
+ingreso_mensual,
+gastos_familiares,
+otros_ingresos,
+replace(replace(replace(ingreso_neto,"$",""), ",",""), ".00","") as ingreso_neto,
+dependientes,
+ventas,
+FLOOR(DATEDIFF(CURDATE(), fecha_nacimiento) / 365.25) AS edad,
+monto_mensual_venta,
+compras,
+delegacion,
+delegacion_negocio,
+escolaridad,
+estado,
+estado_civil,
+estado_negocio,
+sexo,
+sucursal,
+tipo_negocio,
+tipo_vivienda
+from demograficos_distribuidores
